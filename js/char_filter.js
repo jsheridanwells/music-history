3c76157f94a2ae25da03e92814371daa56d7cd10
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-14 14:04:23 -0500

    created functions to print playlists to the page

--- js/char_filter.js
+++ js/char_filter.js
@@ -1,6 +1,9 @@
 // //create container for filtered songs
 let filtered = [];
 
+//select display area for printing playlist
+const main = document.getElementById('main');
+
 //create a regex of characters to filter
 const charsToFilter = /\(|@|!|\*|/g;
 
@@ -29,6 +32,45 @@ for (let i = 0; i < filtered.length; i++) {
 		);
 }
 
-function printPlaylist() {
-	
+//create elements to hold playlist data on page
+function printPlayList(song, artist, album) {
+	let listItems = `
+		<li>${song}</li>
+		<li>${artist}</li>
+		<li>${album}</li>
+	`;
+
+	let list = document.createElement('ul');
+	let header = document.createElement('h3');
+
+	list.innerHTML = listItems;
+	header.innerText = `${song}`
+	main.appendChild(header);
+	main.appendChild(list);
 }
+
+//print playlist data to the page
+for (i = 0; i < playlist.length; i++) {
+	printPlayList(playlist[i].song, playlist[i].artist, playlist[i].album);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
