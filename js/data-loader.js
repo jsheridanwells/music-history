3697881dd1556f8894c32811dbea9bafa2703a24
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-11 16:05:27 -0500

    ajax loaders working

--- js/data-loader.js
+++ js/data-loader.js
@@ -1,41 +1,22 @@
 var Music = (function(obj){
-	let _songData = [];
-	let more = document.getElementById('more');
 
-	obj.loadSongData = (callback, url) => {  //loads data from secified JSON (url)
-		let xhr = new XMLHttpRequest();
-		xhr.addEventListener('load', function() {
-			_songData = JSON.parse(xhr.responseText);
-			callback(_songData);
+	obj.getData = function(data) {
+		let _data = data;
+		Music.showPlaylist(_data);
+	}
+
+	obj.loadData = function(callback, url) {
+		$.ajax({
+			url: url,
+			async: true
+		}).done(function(data){
+			callback(data);
 		});
-		xhr.open('GET', url);
-		xhr.send();
 	};
 
-	obj.showSongs = (data) => {  //inserts data from XHR into page
-		for (let i = 0; i < data.length; i++) {
-			let main = Music.getMain();
-			let mainRow = document.getElementById('main-row');
-			let entry = document.createElement('div');
-			let entryText = `
-			  <h3>${data[i].song}</h3>
-				<ul>
-					<li>${data[i].song}</li>
-					<li>${data[i].artist}</li>
-					<li>${data[i].album}</li>
-				</ul>
-			  `;
-			entry.innerHTML = entryText;
-			main.insertBefore(entry, mainRow);
-		}
-	};
-
-	window.addEventListener('load', function() {  //displays first set of songs on page load
-		Music.loadSongData(Music.showSongs, 'js/songs1.json');
+	$(document).ready(function() {
+		Music.loadData(Music.getData, './js/songs1.json');
 	});
-	more.addEventListener('click', function() {  //displays next set of songs when "more" clicked
-		Music.loadSongData(Music.showSongs, 'js/songs2.json');
-	});
-
 	return obj;
-}(Music || obj));
+
+}(Music || {}));
--- js/ui.js
+++ js/ui.js
@@ -1,24 +1,24 @@
 var Music = (function(obj){
-	let listShow = document.getElementById('list-show');
-	let addShow = document.getElementById('add-show');
-	let list = document.getElementById('list-music-view');
-	let add = document.getElementById('add-music-view');
+	let $mainRow = $('#main-row');
+	obj.showPlaylist = function (data) {
+		let content = '';
+		let $newDiv = $('<div></div');
+		data.forEach((song)=>{
+			content += `
+				<h3>${song.song}</h3>
+				<ul>
+					<li>${song.song}</li>
+					<li>${song.artist}</li>
+					<li>${song.album}</li>
+				</ul>
+			`;
+		});
+		$newDiv.append(content).append($mainRow);
+		$('#main').append($newDiv);
 
-	obj.showAdd = ()=> {  // call to add songs to list
-		listShow.classList.remove('disabled');
-		addShow.classList.add('disabled');
-		list.style = 'display: none';
-		add.style = 'display: flex';
-	}
-
-	obj.showList = ()=> {  // back to song list display
-		listShow.classList.add('disabled');
-		addShow.classList.remove('disabled');
-		list.style = 'display: flex';
-		add.style = 'display: none';
-	}
-
-	addShow.addEventListener('click', obj.showAdd);
-	listShow.addEventListener('click', obj.showList);
+		$mainRow.click(function(){
+			Music.loadData(Music.getData, './js/songs2.json');
+		});
+	};
 	return obj;
 }(Music || {}));
